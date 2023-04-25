a0c55483c6af06512aa2ebdb03c5276781e0882a
Haruki <ldc.illusion99@gmail.com>
2023-04-25 13:37:51 +0700

    Delete RegularPriceCalculator.java

--- RegularPriceCalculator.java
+++
@@ -1,6 +0,0 @@
-public class RegularPriceCalculator implements PriceCalculatorStrategy {
-  @Override
-  public double calculatePrice(Customer customer, Drink drink) {
-    return drink.getPrice();
-  }
-}
